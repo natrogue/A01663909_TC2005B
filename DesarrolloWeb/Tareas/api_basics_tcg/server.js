d782f66738253c688de03cfd5549e21721b6a638
natrogue28 <A01663909@tec.mx>
2024-05-13 13:52:02 -0600

    Correcciones de la API

--- DesarrolloWeb/Tareas/api_basics_tcg/server.js
+++ DesarrolloWeb/Tareas/api_basics_tcg/server.js
@@ -8,7 +8,6 @@ let cartas = [
         "id": 1,
         "type": "Cowboy raccoon",
         "name": "Western Bandit"
-
     },
     {
         "id": 2,
@@ -22,20 +21,6 @@ let cartas = [
     }
 ]
 
-function ValueCard(card){
-    const cardExpected = {
-        "id": "number",
-        "type": "string",
-        "name": "string"
-    
-    }
-    for (let field in cardExpected){
-        if (card.Property(field) && typeof card[field] !== cardExpected[field]){
-            return false
-        }
-    }
-    return true
-}
 // Regresar todas las cartas
 app.get('/cartas', (req, res) => {
     if (cartas.length === 0) {
@@ -54,14 +39,20 @@ app.get('/cartas/:id', (req, res) => {
 
 // Agregar nuevas cartas a la lista
 app.post('/cartas', (req, res) => {
-    const nuevaCarta = req.body;
-    const existeCarta = cartas.find(c => c.id === nuevaCarta.id);
-    if (existeCarta) {
-        res.status(400).json({ message: 'La carta ya existe.' });
-    } else {
-        cartas.push(nuevaCarta);
-        res.json({ message: 'Carta agregada correctamente.' });
-    }
+    const nuevasCartas = Array.isArray(req.body) ? req.body : [req.body];
+    nuevasCartas.forEach(nuevaCarta => {
+        if (!nuevaCarta.id || !nuevaCarta.type || !nuevaCarta.name) {
+            res.status(400).json({ message: 'Faltan atributos en la carta.' });
+        } else {
+            const existeCarta = cartas.find(c => c.id === nuevaCarta.id);
+            if (existeCarta) {
+                res.status(400).json({ message: 'La carta ya existe.' });
+            } else {
+                cartas.push(nuevaCarta);
+            }
+        }
+    });
+    res.json({ message: 'Cartas agregadas correctamente.' });
 });
 
 // Borrar una carta en base a su ID
@@ -82,11 +73,15 @@ app.put('/cartas/:id', (req, res) => {
     if (!carta) {
         res.status(404).send('La carta con el ID dado no existe.');
     } else {
-        Object.assign(carta, req.body);
+        const camposActualizados = req.body;
+        for (let campo in camposActualizados) {
+            if (campo !== 'id' && carta.hasOwnProperty(campo)) {
+                carta[campo] = camposActualizados[campo];
+            }
+        }
         res.send('Carta actualizada.');
     }
 });
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}`));
